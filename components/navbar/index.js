b540496d0d671695295c5d91e03813330e8a97fe
lucas4c <lucas4c@hotmail.com>
2021-06-11 18:48:32 -0300

    third commit, working on navbar, dropdown complete

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,6 +4,24 @@ import styles from './styles/Navbar.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 
+function NavDropdown(props) {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className={styles.dropdownContainer} onMouseEnter={() => setOpen(!open)} onMouseLeave={() => setOpen(!open)}>
+      <span>{props.title}</span>
+        {open && 
+          <div className={styles.dropdown} onMouseLeave={() => setOpen(!open)}>
+            {props.children}
+          </div>
+        }
+        
+    </li>
+  );
+
+}
+
 export default function Navbar({children, ...restProps}) {
     const [navbar, setNavbar] = useState(false);
 
@@ -21,18 +39,25 @@ export default function Navbar({children, ...restProps}) {
 
     return ( 
         <nav className={navbar ? [styles.navbar, styles.active].join(' ') : styles.navbar} {...restProps}>
-        <div className={styles.contact}>
-          <div className={styles.location}>
-            <span>Location</span>
-            <div className={styles.locationMenu}>
-              
-            </div>
-          </div>
-          <div className={styles.language}>
-            <span>Language</span>
-          </div>
-          <Link href="/">Link</Link>
-        </div>
+        <ul className={styles.leftNav}>
+          <li className={styles.contact}>
+            <Link href="/">51 995-672-388</Link>
+          </li>
+          <NavDropdown title="Location">
+            <ul>
+              <li>Rio Grande do Sul</li>
+              <li>Rio de Janeiro</li>
+              <li>São Paulo</li>
+            </ul>
+          </NavDropdown>
+          <NavDropdown title="Language">
+            <ul>
+              <li>Português Brasileiro</li>
+              <li>Inglês</li>
+            </ul>
+          </NavDropdown>
+        </ul>
+        
         <div className={styles.main}>
           <div className={styles.logo}>
             <Image src="/tramanda-logo-dark.svg" layout='fill'></Image>
